3f8c159cac79c40262b3c51be9e406e5897fe829
kviron <kviron.web.dev@gmail.com>
2022-12-26 12:53:33 +0300

    добавление правок структуры темы

--- 404.php
+++ 404.php
@@ -1,22 +1,5 @@
 <?php CE::theLayout('header'); ?>
 
-<div class="top__bg-gradient">
-    <div class="top__bg-box"></div>
-    <section class="page-404">
-        <div class="container">
-            <div class="page-404__inner">
-                <h1 class="page-404__title title-h1">
-                    Потерялись в космосе?
-                </h1>
-            </div>
-        </div>
-        <img class="page-404__img" src="<?= get_theme_file_uri('src/img/astronaut-404.png') ?>" alt="">
-        <div class="container">
-            <a class="page-404__link" href="#">
-                перейти на главную
-            </a>
-        </div>
-    </section>
-</div>
+Ошибка 404
 
 <?php CE::theLayout('footer'); ?>
